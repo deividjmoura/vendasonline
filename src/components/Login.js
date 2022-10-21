b27091efbd882fbe779ddb9ded59e5ac4e6c9f76
Deivid <dejesusdemouradeivid@gmail.com>
2022-10-21 11:08:50 -0300

    feat(login.js): creating a login page
    intruduting a new login page

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import { Box, Flex, Spacer } from '@chakra-ui/react';
+
+const Login = () => {
+    return (
+    <Flex>
+  <Box p='4' bg='red.400'>
+    Box 1
+  </Box>
+  <Spacer />
+  <Box p='4' bg='green.400'>
+    Box 2
+  </Box>
+</Flex>
+)};
+export default Login;
